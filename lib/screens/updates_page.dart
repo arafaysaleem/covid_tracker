3e8c0647ecf683bcddf88d7c702ef6bf16ccbfdc
arafaysaleem <a.rafaysaleem@gmail.com>
2020-05-05 00:15:54 +0500

    improved doropdown

--- lib/screens/updates_page.dart
+++ lib/screens/updates_page.dart
@@ -16,7 +16,6 @@ class _UpdatesScreenState extends State<UpdatesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
       appBar: AppBar(
         title: Text(
           "Covid-19 Updates",
@@ -55,118 +54,134 @@ class _UpdatesScreenState extends State<UpdatesScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              //Divider + DropDown
+              //Divider
               Container(
                 color: Colors.black,
                 height: 2,
               ),
+
               SizedBox(height: 15),
 
-              //DropDown
-              Container(
-                width: 200,
-                padding: const EdgeInsets.only(left: 5, right: 10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                height: 55,
-                child: Center(
-                  child: Theme(
-                    data: ThemeData(
-                      canvasColor: Colors.white,
+              //Sorting
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+
+                  Text(
+                    "Sort By",
+                    style: TextStyle(
+                      fontFamily: "Monsterrat",
+                      fontSize: 17,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
                     ),
-                    child: ButtonTheme(
-                      alignedDropdown: true,
-                      child: DropdownButton<String>(
-                        itemHeight: 50,
-                        value: dropDownValue,
-                        style: TextStyle(
-                          fontFamily: "Monsterrat",
-                          fontSize: 19,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w600,
+                  ),
+
+                  SizedBox(width:10),
+
+                  Icon(Icons.filter_list),
+
+                  SizedBox(width:15),
+
+                  //DropDown
+                  Container(
+                    width: 200,
+                    padding: const EdgeInsets.only(left: 15, right: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    height: 55,
+                    child: Center(
+                      child: Theme(
+                        data: ThemeData(
+                          canvasColor: Colors.black,
                         ),
-                        underline: Container(
-                          height: 0,
+                        child: DropdownButton<String>(
+                          isExpanded: true,
+                          itemHeight: 50,
+                          value: dropDownValue,
+                          underline: Container(
+                            height: 0,
+                          ),
+                          elevation: 20,
+                          iconSize: 28,
+                          icon: Icon(
+                            Icons.expand_more,
+                            color: Colors.white,
+                          ),
+                          items: <DropdownMenuItem<String>>[
+                            DropdownMenuItem(
+                              value: "publishedAt",
+                              child: Text(
+                                "Published Date",
+                                style: TextStyle(
+                                  fontFamily: "Monsterrat",
+                                  fontSize: 17,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: "Latest",
+                              child: Text(
+                                "Latest",
+                                style: TextStyle(
+                                  fontFamily: "Monsterrat",
+                                  fontSize: 17,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: "Last Week",
+                              child: Text(
+                                "Last Week",
+                                style: TextStyle(
+                                  fontFamily: "Monsterrat",
+                                  fontSize: 17,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: "Last 15",
+                              child: Text(
+                                "Last 15 days",
+                                style: TextStyle(
+                                  fontFamily: "Monsterrat",
+                                  fontSize: 17,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: "Last Month",
+                              child: Text(
+                                "Last Month",
+                                style: TextStyle(
+                                  fontFamily: "Monsterrat",
+                                  fontSize: 17,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ],
+                          onChanged: (String newValue) {
+                            setState(() {
+                              dropDownValue = newValue;
+                            });
+                          },
                         ),
-                        elevation: 0,
-                        iconSize: 28,
-                        icon: Icon(
-                          Icons.expand_more,
-                          color: Colors.black,
-                        ),
-                        items: <DropdownMenuItem<String>>[
-                          DropdownMenuItem(
-                            value: "publishedAt",
-                            child: Text(
-                              "Published Date",
-                              style: TextStyle(
-                                fontFamily: "Monsterrat",
-                                fontSize: 19,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          DropdownMenuItem(
-                            value: "Latest",
-                            child: Text(
-                              "Latest",
-                              style: TextStyle(
-                                fontFamily: "Monsterrat",
-                                fontSize: 19,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          DropdownMenuItem(
-                            value: "Last Week",
-                            child: Text(
-                              "Last Week",
-                              style: TextStyle(
-                                fontFamily: "Monsterrat",
-                                fontSize: 19,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          DropdownMenuItem(
-                            value: "Last 15",
-                            child: Text(
-                              "Last 15 days",
-                              style: TextStyle(
-                                fontFamily: "Monsterrat",
-                                fontSize: 19,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          DropdownMenuItem(
-                            value: "Last Month",
-                            child: Text(
-                              "Last Month",
-                              style: TextStyle(
-                                fontFamily: "Monsterrat",
-                                fontSize: 19,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ],
-                        onChanged: (String newValue) {
-                          setState(() {
-                            dropDownValue = newValue;
-                          });
-                        },
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
             ],
           ),
