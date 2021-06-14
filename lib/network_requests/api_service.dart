9eed744ac8de8212a3c3b895adeb9280143c5b92
Abdur Rafay Saleem <62943972+arafaysaleem@users.noreply.github.com>
2021-06-14 15:09:31 +0500

    Remove extra semi-colon from ApiService

--- lib/network_requests/api_service.dart
+++ lib/network_requests/api_service.dart
@@ -1,5 +1,5 @@
 class ApiService{
-  static const String apiKey=String.fromEnvironment('API_KEY', defaultValue: '');;
+  static const String apiKey=String.fromEnvironment('API_KEY', defaultValue: '');
   final String _newsUrl="http://newsapi.org/v2/everything?";
   final String _statsUrl="https://disease.sh/v2/";
   final String _query="q=covid%2019%20vaccine";
