b57ed5e5712743144042c0460e2e47bd33d650f4
Abdur Rafay Saleem <62943972+arafaysaleem@users.noreply.github.com>
2021-06-14 03:03:45 -0700

    Removed getter for API_KEY class member

--- lib/network_requests/api_service.dart
+++ lib/network_requests/api_service.dart
@@ -1,11 +1,10 @@
 class ApiService{
-  static const String _apiKey=String.fromEnvironment('API_KEY', defaultValue: '');;
+  static const String apiKey=String.fromEnvironment('API_KEY', defaultValue: '');;
   final String _newsUrl="http://newsapi.org/v2/everything?";
   final String _statsUrl="https://disease.sh/v2/";
   final String _query="q=covid%2019%20vaccine";
   final String _limit="pageSize=10&page=1";
 
-  String get apiKey => _apiKey;
   String get query => _query;
   String get newsUrl => _newsUrl;
   String get statsUrl => _statsUrl;
